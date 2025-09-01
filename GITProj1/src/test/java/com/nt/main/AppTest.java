48c6a3581828771c3c37511eafd1c0510f1dc43c
BHUPAL REDDY <BHUPAL REDDY@172.16.0.67>
2025-09-01 15:23:17 +0530

    task 1 by developer1

--- GITProj1/src/test/java/com/nt/main/AppTest.java
+++ GITProj1/src/test/java/com/nt/main/AppTest.java
@@ -1,19 +1,31 @@
 package com.nt.main;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+   @Test
+    public void testsumwithPositives() {
+       App app=new App();
+       int excepted=300;
+       int actual=app.sum(100, 200);
+       assertEquals(excepted,actual);
     }
+   @Test
+   public void testsumwithNegitives() {
+      App app=new App();
+      int excepted=-300;
+      int actual=app.sum(-100, -200);
+      assertEquals(excepted,actual);
+   }
+   @Test
+   public void testsumwithMixedValues() {
+      App app=new App();
+      int excepted=100;
+      int actual=app.sum(-100, 200);
+      assertEquals(excepted,actual);
+   }
+
 }
