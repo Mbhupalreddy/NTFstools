1da28582543054277cf158142d8293b193d041c4
BHUPAL REDDY <BHUPAL REDDY@LAPTOP-6APJ1DHF>
2025-09-05 23:04:02 +0530

    TL enhancing the branch1 code

--- GITProj1/src/main/java/com/nt/upi/UPIPayment.java
+++ GITProj1/src/main/java/com/nt/upi/UPIPayment.java
@@ -1,8 +1,13 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class UPIPayment {
 	
-	public String doUPIPayment(String upiid,float amount) {
+	public String doUPIPayment(String upiid,double amount) {
 		return "Upi payment for the amount::"+amount+"is done";
+	}
+	public double checkBalance(String upid) {
+		return new Random().nextInt(1200000);
 	}
 }
