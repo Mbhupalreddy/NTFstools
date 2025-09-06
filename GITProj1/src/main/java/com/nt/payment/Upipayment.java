c1cf65da36f4b5010fcf41aef3edaa199882b644
BHUPAL REDDY <BHUPAL REDDY@LAPTOP-6APJ1DHF>
2025-09-06 22:01:51 +0530

    create new branch-by TL

---
+++ GITProj1/src/main/java/com/nt/payment/Upipayment.java
@@ -0,0 +1,14 @@
+package com.nt.payment;
+
+public class Upipayment {
+	public String BankPayment(double  amount,double increment) {
+		return "bhupal";
+	}
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
